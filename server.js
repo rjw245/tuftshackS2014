2e2f28fb160c2e5fdbbe7eb9adac9f3eb7cf19ee
Riley <riley@ubuntu.(none)>
2014-02-01 20:36:56 -0500

    i think i changed something

--- server.js
+++ server.js
@@ -11,23 +11,7 @@ var socketio = require('socket.io');
 var express = require('express');
 
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('mydb.db');
-
-db.serialize(function() {
-  db.run("CREATE TABLE lorem (info TEXT)");
-
-  var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-  for (var i = 0; i < 10; i++) {
-      stmt.run("Ipsum " + i);
-  }
-  stmt.finalize();
-
-  db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
-      console.log(row.id + ": " + row.info);
-  });
-});
-
-db.close();
+var db = new sqlite3.Database('database.db');
 
 //
 // ## SimpleServer `SimpleServer(obj)`
@@ -70,12 +54,23 @@ io.on('connection', function (socket) {
         broadcast('message', data);
         messages.push(data);
       });
+
+      //INSERT IN DATABASE
+      db.serialize(function() {
+        var time = (new Date()).getTime();
+        var stmt = db.prepare("INSERT INTO messages VALUES(NULL,'1','THIS IS MY SUBJECT',?,"+time+",'2')");
+        stmt.run(msg);
+        stmt.finalize();
+      });
+      //db.close();
+
     });
 
     socket.on('identify', function (name) {
       socket.set('name', String(name || 'Anonymous'), function (err) {
         updateRoster();
       });
+
     });
   });
 
