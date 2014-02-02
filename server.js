2199957b27e350701826f00c9a52bd2dafa54114
Riley <riley@ubuntu.(none)>
2014-02-01 19:40:23 -0500

    renamed db

--- server.js
+++ server.js
@@ -10,6 +10,25 @@ var async = require('async');
 var socketio = require('socket.io');
 var express = require('express');
 
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('mydb.db');
+
+db.serialize(function() {
+  db.run("CREATE TABLE lorem (info TEXT)");
+
+  var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+  for (var i = 0; i < 10; i++) {
+      stmt.run("Ipsum " + i);
+  }
+  stmt.finalize();
+
+  db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
+      console.log(row.id + ": " + row.info);
+  });
+});
+
+db.close();
+
 //
 // ## SimpleServer `SimpleServer(obj)`
 //
